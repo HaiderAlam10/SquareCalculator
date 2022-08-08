afce8b02888491cff0a35b8556525015269d0df9
HaiderAlam10 <alamhaider1@gmail.com>
2022-08-08 19:58:29 +0100

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double width = (13.5);
+        double height = (25.3);
+        System.out.println(width);
+        System.out.println(height);
+        double perimeter = (width*2);
+        double perimeter2 = (height*2);
+        System.out.println("perimeter of rectangle is: " +perimeter2 + perimeter);
+    }
+}
---
+++ src/SquareCalculator.java
@@ -0,0 +1,14 @@
+public class SquareCalculator {
+
+    public static void main(String[] args) {
+        double width = (13.5);
+        double height = (25.3);
+        System.out.println(13.5);
+        System.out.println(25.3);
+        double perimeter = 2 * width + 2 * height;
+        System.out.println("perimeter of rectangle is: " +perimeter);
+
+        double area = (width*height);
+        System.out.println("area is" +area);
+    }
+}
